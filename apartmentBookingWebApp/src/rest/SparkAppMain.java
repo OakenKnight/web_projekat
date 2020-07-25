0a58b0ef9249d2e49d8f8d65d710dbbdc368856e
radovan-zupunski <rzupunski@gmail.com>
2020-07-25 10:56:39 +0200

    test

--- apartmentBookingWebApp/src/rest/SparkAppMain.java
+++ apartmentBookingWebApp/src/rest/SparkAppMain.java
@@ -1,12 +1,25 @@
 package rest;
 import static spark.Spark.*;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class SparkAppMain {
 
 	public static void main(String[] args) {
-		port(8080);
-
+		port(5000);
+		
+		try {
+			staticFiles.externalLocation(new File("./static").getCanonicalPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		get("rest/test", (req, res) -> {
+			return "Radi";
+		});
+		
 	}
 
 }
